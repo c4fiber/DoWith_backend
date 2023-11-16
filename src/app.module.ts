b469cf9326f1e6e51e62a3de31d8f8456e414b98
LeeInBok <bbock1214@gmail.com>
2023-11-16 18:12:49 +0900

    before work for Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DoWithMiddlewareMiddleware } from 'src/do-with-middleware/do-with-middleware.middleware';
 import { DoWithExceptionFilterModule } from './do-with-exception-filter/do-with-exception-filter.module';
 import { DoWithExceptionModule } from './do-with-exception/do-with-exception.module';
+import { GroupModule } from './group/group.module';
 
 // timezone check
 const now = new Date();
@@ -16,27 +17,29 @@ console.log(new Date().toISOString());
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal   : true,
+      envFilePath: `./env/.${process.env.NODE_ENV}.env`
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: 'dowith-db',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
       extra: {
         timezone: 'Asia/Seoul'
       }
     }),
+    // Common Module
     DoWithExceptionModule,
     DoWithExceptionFilterModule,
-    ConfigModule.forRoot({
-      isGlobal   : true,
-      envFilePath: `./env/.${process.env.NODE_ENV}.env`
-    }),
     // API Module
-    TodoModule
+    TodoModule,
+    GroupModule
   ],
   controllers: [AppController],
   providers: [AppService],
