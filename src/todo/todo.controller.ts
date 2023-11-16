60f3174c8c910a02a524b8a30354e2c382c451b5
c4fiber <qudcjf153@gmail.com>
2023-11-16 21:18:12 +0900

    refactor: formatter 수행

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,41 +1,59 @@
-import {Get, Post, Patch, Put, Delete, Controller, Logger, Body, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Controller,
+  Logger,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { Todo } from './todo.entity';
 
 @Controller('todo')
 export class TodoController {
-    private logger = new Logger('Todos');
-    constructor(private todoService: TodoService) {}
+  private logger = new Logger('Todos');
+  constructor(private todoService: TodoService) {}
 
-    @Get('/user/:user_id')
-    findAllByUser(@Param('user_id', ParseIntPipe) user_id: number): Promise<Todo[]> {
-        return this.todoService.findAllByUser(user_id);
-    }
+  @Get('/user/:user_id')
+  findAllByUser(
+    @Param('user_id', ParseIntPipe) user_id: number,
+  ): Promise<Todo[]> {
+    return this.todoService.findAllByUser(user_id);
+  }
 
-    @Get('/:todo_id')
-    findOne(@Param('todo_id', ParseIntPipe) todo_id: number): Promise<Todo> {
-        return this.todoService.findOne(todo_id);
-    }
+  @Get('/:todo_id')
+  findOne(@Param('todo_id', ParseIntPipe) todo_id: number): Promise<Todo> {
+    return this.todoService.findOne(todo_id);
+  }
 
-    @Post()
-    create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
-        return this.todoService.create(createTodoDto);
-    }
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoService.create(createTodoDto);
+  }
 
-    @Put('/:todo_id')
-    update(@Param('todo_id', ParseIntPipe) todo_id: number, @Body() createTodoDto: CreateTodoDto): Promise<Todo> {
-        return this.todoService.update(todo_id, createTodoDto);
-    }
+  @Put('/:todo_id')
+  update(
+    @Param('todo_id', ParseIntPipe) todo_id: number,
+    @Body() createTodoDto: CreateTodoDto,
+  ): Promise<Todo> {
+    return this.todoService.update(todo_id, createTodoDto);
+  }
 
-    @Delete('/:todo_id')
-    delete(@Param('todo_id', ParseIntPipe) todo_id: number): Promise<void> {
-        return this.todoService.delete(todo_id);
-    }
+  @Delete('/:todo_id')
+  delete(@Param('todo_id', ParseIntPipe) todo_id: number): Promise<void> {
+    return this.todoService.delete(todo_id);
+  }
 
-    @Patch('/:todo_id')
-    editDone(@Param('todo_id', ParseIntPipe) todo_id: number, @Body() createTodoDto: CreateTodoDto): Promise<Todo> {
-        return this.todoService.editDone(todo_id, createTodoDto);
-    }
-
+  @Patch('/:todo_id')
+  editDone(
+    @Param('todo_id', ParseIntPipe) todo_id: number,
+    @Body() createTodoDto: CreateTodoDto,
+  ): Promise<Todo> {
+    return this.todoService.editDone(todo_id, createTodoDto);
+  }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,61 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm'
-import { Todo } from './todo.entity'
-import { CreateTodoDto } from './dto/create-todo.dto'
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
-    constructor(
-        @InjectRepository(Todo)
-        private readonly todoRepository: Repository<Todo>,
-    ) {}
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todoRepository: Repository<Todo>,
+  ) {}
 
-    // READ
-    async findAllByUser(user_id: number): Promise <Todo[]> {
-        return await this.todoRepository.findBy({ user_id });
-    }
+  // READ
+  async findAllByUser(user_id: number): Promise<Todo[]> {
+    return await this.todoRepository.findBy({ user_id });
+  }
 
-    async findOne(todo_id: number): Promise <Todo> {
-        return await this.todoRepository.findOneBy({ todo_id });
-    }
+  async findOne(todo_id: number): Promise<Todo> {
+    return await this.todoRepository.findOneBy({ todo_id });
+  }
 
-    // CREATE
-    async create(createTodoDto: CreateTodoDto): Promise <Todo> {
-        const todo = new Todo();
-        todo.user_id = createTodoDto.user_id;
-        todo.todo_name = createTodoDto.todo_name;
-        todo.todo_desc = createTodoDto.todo_desc;
-        todo.todo_done = createTodoDto.todo_done;
-        todo.todo_start = createTodoDto.todo_start;
-        todo.todo_end = createTodoDto.todo_end;
-        todo.grp_id = createTodoDto.grp_id;
-        todo.todo_img = createTodoDto.todo_img;
+  // CREATE
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const todo = new Todo();
+    todo.user_id = createTodoDto.user_id;
+    todo.todo_name = createTodoDto.todo_name;
+    todo.todo_desc = createTodoDto.todo_desc;
+    todo.todo_done = createTodoDto.todo_done;
+    todo.todo_start = createTodoDto.todo_start;
+    todo.todo_end = createTodoDto.todo_end;
+    todo.grp_id = createTodoDto.grp_id;
+    todo.todo_img = createTodoDto.todo_img;
 
-        return await this.todoRepository.save(todo);
-    }
+    return await this.todoRepository.save(todo);
+  }
 
-    // UPDATE
-    async update(todo_id: number, createTodoDto: CreateTodoDto): Promise <Todo> {
-        const todo = await this.todoRepository.findOneBy({ todo_id });
-        todo.user_id = createTodoDto.user_id;
-        todo.todo_name = createTodoDto.todo_name;
-        todo.todo_desc = createTodoDto.todo_desc;
-        todo.todo_done = createTodoDto.todo_done;
-        todo.todo_start = createTodoDto.todo_start;
-        todo.todo_end = createTodoDto.todo_end;
-        todo.grp_id = createTodoDto.grp_id;
-        todo.todo_img = createTodoDto.todo_img;
+  // UPDATE
+  async update(todo_id: number, createTodoDto: CreateTodoDto): Promise<Todo> {
+    const todo = await this.todoRepository.findOneBy({ todo_id });
+    todo.user_id = createTodoDto.user_id;
+    todo.todo_name = createTodoDto.todo_name;
+    todo.todo_desc = createTodoDto.todo_desc;
+    todo.todo_done = createTodoDto.todo_done;
+    todo.todo_start = createTodoDto.todo_start;
+    todo.todo_end = createTodoDto.todo_end;
+    todo.grp_id = createTodoDto.grp_id;
+    todo.todo_img = createTodoDto.todo_img;
 
-        return await this.todoRepository.save(todo);
-    }
+    return await this.todoRepository.save(todo);
+  }
 
-    // DELETE
-    async delete(todo_id: number): Promise <void> {
-        await await this.todoRepository.delete({ todo_id });
-    }
+  // DELETE
+  async delete(todo_id: number): Promise<void> {
+    await await this.todoRepository.delete({ todo_id });
+  }
 
-      // todo 완료상태 변경
+  // todo 완료상태 변경
   async editDone(todo_id: number, createTodoDto: CreateTodoDto): Promise<Todo> {
     const todo = await this.todoRepository.findOneBy({ todo_id });
     todo.todo_done = createTodoDto.todo_done;
