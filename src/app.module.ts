e793f0832b0bdda4e2960d017dd6a79403a21349
c4fiber <qudcjf153@gmail.com>
2023-11-16 11:40:23 +0900

    feat: DB 연결 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: 'dowith-db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
