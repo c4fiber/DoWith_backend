0d16c529b7b39510a339f2c41f98079d26e5619e
c4fiber <qudcjf153@gmail.com>
2023-11-19 21:32:05 +0900

    fix: todo dto 수정(create)

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -4,20 +4,19 @@ export class CreateTodoDto {
   @IsNotEmpty()
   user_id: number; // foreign key
 
+  // essential
   @IsNotEmpty()
   todo_name: string;
-
   todo_desc: string;
-
-  @IsNotEmpty()
+  todo_label: number;
+  todo_date: Date;
   todo_done: boolean;
 
+  // optional
   todo_start: Date;
-
   todo_end: Date;
-
   grp_id: number; // foreign key
-
   // path of image, nessary if grp_id is not null
   todo_img: string;
+
 }
