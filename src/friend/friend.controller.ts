d75744391e4a3efc18bc8e9a35f57cbaa21373d9
cece <sojeong.lee017@gmail.com>
2023-11-21 02:27:27 +0900

    [IWW-73] feat: freind api

--- src/friend/friend.controller.ts
+++ src/friend/friend.controller.ts
@@ -12,24 +12,28 @@ import {
 import { UserService } from 'src/user/user.service';
 import { FriendService } from './friend.service';
 import { FreindRequestDto } from './dto/friend-request.dto';
+import { UserResponseDto } from 'src/user/dto/user-response.dto';
 
 @Controller('friend')
 export class FriendController {
   constructor(private readonly friendService: FriendService) {}
 
   @Get('/:user_id')
-  async getFriends(@Param('user_id', ParseIntPipe) id: number) {
+  async getFriends(
+    @Param('user_id', ParseIntPipe) id: number,
+  ): Promise<UserResponseDto[]> {
     return await this.friendService.getFriends(id);
   }
 
   @Post('/')
   @UsePipes(ValidationPipe)
-  async createFriend(@Body() body: FreindRequestDto) {
-    return await this.createFriend(body);
+  async createFriend(@Body() body: FreindRequestDto): Promise<boolean> {
+    console.log(`🔥 ${JSON.stringify(body)}`);
+    return await this.friendService.createFriend(body);
   }
 
   @Delete('/')
-  async deleteFriend(@Body() body: FreindRequestDto) {
-    return await this.deleteFriend(body);
+  async deleteFriend(@Body() body: FreindRequestDto): Promise<boolean> {
+    return await this.friendService.deleteFriend(body);
   }
 }
--- src/friend/friend.service.ts
+++ src/friend/friend.service.ts
@@ -11,21 +11,39 @@ export class FriendService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    private readonly doWithException: DoWithExceptions,
+    private readonly doWithExceptions: DoWithExceptions,
   ) {}
 
   // TODO: 예외처리들..
   // 친구 조회
   async getFriends(id: number): Promise<UserResponseDto[]> {
-    const user = await this.userRepository.findOne({
+    const user: User = await this.userRepository.findOne({
       where: { user_id: id },
+      relations: ['friends'],
     });
+
+    // 유저가 없는 경우
+    if (user == null) {
+      throw this.doWithExceptions.UserNotFound;
+    }
+
+    console.log(user.friends);
+
+    // 친구가 없는 경우
+    if (user.friends.length == 0) return [];
+
+    // 결과 리턴
     return user.friends.map((friend) => new UserResponseDto(friend));
   }
 
   // 친구 추가
-  async createFriend(body: FreindRequestDto): Promise<UserResponseDto> {
+  async createFriend(body: FreindRequestDto): Promise<boolean> {
     const { user_id, friend_id } = body;
+
+    // 자기 자신과 친구 요청 예외처리
+    if (user_id === friend_id) {
+      throw this.doWithExceptions.SelfFriendship;
+    }
 
     const user = await this.userRepository.findOne({
       where: { user_id: user_id },
@@ -36,25 +54,31 @@ export class FriendService {
       where: { user_id: friend_id },
     });
 
-    // 자기 자신과 친구 맺기 요청 예외처리
-    if (user.user_id === friend.user_id) {
-      throw this.doWithException.SelfFriendship;
-    }
-
     user.friends.push(friend);
     return this.userRepository
       .save(user)
-      .then((user) => new UserResponseDto(user));
+      .then((user) => new UserResponseDto(user))
+      .then((_) => true)
+      .catch((_) => false);
   }
 
   // 친구 삭제
-  async deleteFriend(body: FreindRequestDto): Promise<void> {
+  async deleteFriend(body: FreindRequestDto): Promise<boolean> {
     const { user_id, friend_id } = body;
+    // 자기 자신과 친구 요청 예외처리
+    if (user_id === friend_id) {
+      throw this.doWithExceptions.SelfFriendship;
+    }
+
     const user = await this.userRepository.findOne({
       where: { user_id: user_id },
       relations: ['friends'],
     });
+
     user.friends = user.friends.filter((friend) => friend.user_id != friend_id);
-    await this.userRepository.save(user);
+    return await this.userRepository
+      .save(user)
+      .then((_) => true)
+      .catch((_) => false);
   }
 }
