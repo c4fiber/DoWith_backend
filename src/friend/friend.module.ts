63d71fc1454dda1a0c68899af810f2a94fb4287e
cece <sojeong.lee017@gmail.com>
2023-11-21 02:09:10 +0900

    [IWW-73] fix: friend module error

--- src/friend/friend.module.ts
+++ src/friend/friend.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FriendController } from './friend.controller';
 import { FriendService } from './friend.service';
-import { UserService } from 'src/user/user.service';
+import { DoWithExceptions } from 'src/do-with-exception/do-with-exception';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [FriendController],
-  providers: [FriendService, UserService],
+  providers: [FriendService, DoWithExceptions],
 })
 export class FriendModule {}
