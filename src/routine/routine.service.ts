7bac6277c43735879b8b9c9cf22612abcc9cd7e2
c4fiber <qudcjf153@gmail.com>
2023-11-24 23:11:15 +0900

    fix더미 데이터 생성 오류 수정

--- src/routine/routine.service.ts
+++ src/routine/routine.service.ts
@@ -10,40 +10,44 @@ import { Todo } from 'src/todo/todo.entity';
 @Injectable()
 export class RoutineService {
   constructor(
-    @InjectRepository(Routine) private readonly routineRepository: Repository<Routine>,
+    @InjectRepository(Routine)
+    private readonly routineRepository: Repository<Routine>,
     private readonly dowithException: DoWithExceptions,
     private dataSource: DataSource,
-    private readonly logger: Logger
+    private readonly logger: Logger,
   ) {}
 
-  async getAllRoutines(grp_id: number): Promise<any>{
-    const result = await this.routineRepository.createQueryBuilder('r')
-                                               .leftJoin('group', 'g', 'g.grp_id = r.grp_id')
-                                               .where('g.grp_id = :grp_id', {grp_id})
-                                               .select([
-                                                 'r.grp_id      AS grp_id'
-                                                 , 'r.rout_name   AS rout_name'
-                                                 , 'r.rout_desc   AS rout_desc'
-                                                 , 'r.rout_repeat AS rout_repeat'
-                                                 , 'r.rout_srt    AS rout_srt'
-                                                 , 'r.rout_end    AS rout_end'
-                                               ])
-                                               .getRawMany();
+  async getAllRoutines(grp_id: number): Promise<any> {
+    const result = await this.routineRepository
+      .createQueryBuilder('r')
+      .leftJoin('group', 'g', 'g.grp_id = r.grp_id')
+      .where('g.grp_id = :grp_id', { grp_id })
+      .select([
+        'r.grp_id      AS grp_id',
+        'r.rout_name   AS rout_name',
+        'r.rout_desc   AS rout_desc',
+        'r.rout_repeat AS rout_repeat',
+        'r.rout_srt    AS rout_srt',
+        'r.rout_end    AS rout_end',
+      ])
+      .getRawMany();
     this.logger.debug(result);
-    
+
     return result;
   }
 
-  async createRoutine(grp_id: number, createRoutineDto: CreateRoutineDto): Promise<any> {
-    const routList = await this.routineRepository.createQueryBuilder('r')
-                                                 .leftJoin('group', 'g', 'g.grp_id = r.grp_id')
-                                                 .where('g.grp_id = :grp_id', {grp_id})
-                                                 .select([
-                                                   'COUNT(*) AS cnt'
-                                                 ])
-                                                 .getRawOne();
+  async createRoutine(
+    grp_id: number,
+    createRoutineDto: CreateRoutineDto,
+  ): Promise<any> {
+    const routList = await this.routineRepository
+      .createQueryBuilder('r')
+      .leftJoin('group', 'g', 'g.grp_id = r.grp_id')
+      .where('g.grp_id = :grp_id', { grp_id })
+      .select(['COUNT(*) AS cnt'])
+      .getRawOne();
 
-    if(routList.cnt >= 3){
+    if (routList.cnt >= 3) {
       throw this.dowithException.ExceedMaxRoutines;
     }
 
@@ -52,18 +56,19 @@ export class RoutineService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
-    try{
+    try {
       createRoutineDto['grp_id'] = grp_id;
 
       const routIns = await queryRunner.manager.save(Routine, createRoutineDto);
-      const memList = await queryRunner.manager.createQueryBuilder()
-                                               .select('ug.user_id AS user_id')
-                                               .from(Group, 'g')
-                                               .leftJoin('user_group', 'ug', 'g.grp_id = ug.grp_id')
-                                               .where('g.grp_id = :grp_id', { grp_id })
-                                               .getRawMany();
+      const memList = await queryRunner.manager
+        .createQueryBuilder()
+        .select('ug.user_id AS user_id')
+        .from(Group, 'g')
+        .leftJoin('user_group', 'ug', 'g.grp_id = ug.grp_id')
+        .where('g.grp_id = :grp_id', { grp_id })
+        .getRawMany();
       this.logger.debug(routIns);
-      memList.forEach(async (data) => { 
+      memList.forEach(async (data) => {
         const todo = new Todo();
         // local inmemory에서 해결할 루틴으로 To-Do 만들기임
         todo['user_id'] = data.user_id;
@@ -72,7 +77,7 @@ export class RoutineService {
         todo.todo_desc = routIns.rout_desc;
         todo.todo_start = routIns.rout_srt;
         todo.todo_end = routIns.rout_end;
-        todo.todo_label = '2020-11-11 11:11:11';
+        todo.todo_label = 0;
 
         await queryRunner.manager.save(Todo, todo);
       });
@@ -80,8 +85,7 @@ export class RoutineService {
       //return;
       await queryRunner.commitTransaction();
       return;
-
-    } catch(err){
+    } catch (err) {
       this.logger.error(err);
       await queryRunner.rollbackTransaction();
     }
