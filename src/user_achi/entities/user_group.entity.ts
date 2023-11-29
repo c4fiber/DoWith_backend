99548ab55971b000753f5461ad41b5f2977e8ebe
LeeInBok <bbock1214@gmail.com>
2023-11-29 13:12:28 +0900

    feat: achievemet

--- src/user_achi/entities/user_group.entity.ts
+++ src/user_achi/entities/user_group.entity.ts
@@ -1,10 +1,10 @@
 import { Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
-export class UserGroup {
+export class UserAchi {
   @PrimaryColumn()
   user_id: number;
 
   @PrimaryColumn()
-  ache_id: number;
+  achi_id: number;
 }
