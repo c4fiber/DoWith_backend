314f0c88d16425da36cae9d6ddc398a5a3b0996e
c4fiber <qudcjf153@gmail.com>
2023-12-03 20:50:20 +0900

    fix: myroom API 변경

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,6 +1,7 @@
 import {
   ConflictException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -32,21 +33,49 @@ export class RoomService {
     return result1 !== null;
   }
 
-  // for controller
-  async create(user_id: number, item_id: number): Promise<any> {
-    // 보유한 아이템이 아니다
-    if (!(await this.isInInventory(user_id, item_id))) {
-      throw this.doWithException.ItemNotInInventory;
-    }
-    // 이미 방에 있는 아이템이다.
-    if (await this.isInMyRoom(user_id, item_id)) {
-      throw this.doWithException.ItemAlreadyInMyRoom;
-    }
+  async overlap(user_id: number, items: number[]): Promise<any> {
+    // 기존의 room아이템 모두 삭제
+    await this.roomRepo.delete({ user_id });
 
-    // TODO 펫이 여러마리 들어가면 안된다.
+    // 새로운 room아이템 추가
+    const result = this.dataSource.manager
+      .transaction(async (manager) => {
+        for (const item_id of items) {
+          if (!(await this.isInInventory(user_id, item_id))) {
+            throw this.doWithException.ItemNotInInventory;
+          }
+          if (await this.isInMyRoom(user_id, item_id)) {
+            throw this.doWithException.ItemAlreadyInMyRoom;
+          }
 
-    return { result: await this.roomRepo.save({ user_id, item_id }) };
+          // 보유한 아이템 + 방에 없음 -> 방에 추가
+          await this.roomRepo.save({ user_id, item_id });
+        }
+      })
+      .catch((err) => {
+        throw new InternalServerErrorException(
+          '데이터를 삽입하는 과정에서 문제가 발생했습니다.',
+        );
+      });
+
+    return { result };
   }
+
+  // // for controller
+  // async create(user_id: number, item_id: number): Promise<any> {
+  //   // 보유한 아이템이 아니다
+  //   if (!(await this.isInInventory(user_id, item_id))) {
+  //     throw this.doWithException.ItemNotInInventory;
+  //   }
+  //   // 이미 방에 있는 아이템이다.
+  //   if (await this.isInMyRoom(user_id, item_id)) {
+  //     throw this.doWithException.ItemAlreadyInMyRoom;
+  //   }
+
+  //   // TODO 펫이 여러마리 들어가면 안된다.
+
+  //   return { result: await this.roomRepo.save({ user_id, item_id }) };
+  // }
 
   async findAll(user_id: number) {
     const result = await this.roomRepo
@@ -66,13 +95,13 @@ export class RoomService {
     return { result };
   }
 
-  async remove(user_id: number, item_id: number) {
-    // 방에 아이템이 존재하지 않으면 에러
-    if (!(await this.isInMyRoom(user_id, item_id))) {
-      throw new NotFoundException();
-    }
+  // async remove(user_id: number, item_id: number) {
+  //   // 방에 아이템이 존재하지 않으면 에러
+  //   if (!(await this.isInMyRoom(user_id, item_id))) {
+  //     throw new NotFoundException();
+  //   }
 
-    const result = await this.roomRepo.delete({ user_id, item_id });
-    return { result };
-  }
+  //   const result = await this.roomRepo.delete({ user_id, item_id });
+  //   return { result };
+  // }
 }
