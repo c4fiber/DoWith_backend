169a348800c0adc20c6a089ef6e31f3e601af0d6
coding-jjun <psjbrian@gmail.com>
2023-12-04 04:34:22 +0900

    fix: module import

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppGateway } from "./app.gateway";
+import { UserModule } from 'src/user/user.module';
+import { GroupModule } from 'src/group/group.module';
+import { TodoModule } from 'src/todo/todo.module';
+import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
+    imports: [UserModule, GroupModule, TodoModule, NotificationModule],
     providers: [AppGateway],
     exports: [AppGateway],
 })
