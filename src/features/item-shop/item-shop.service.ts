ef4fd396167616eedd3d574650e7d297eb7e0f52
LeeInBok <bbock1214@gmail.com>
2023-12-03 19:07:03 +0900

    chore: comment in Item-Shop File

--- src/features/item-shop/item-shop.service.ts
+++ src/features/item-shop/item-shop.service.ts
@@ -11,45 +11,53 @@ import { ItemInventory } from 'src/entities/item-inventory.entity';
 export class ItemShopService {
   constructor(
     @InjectRepository(ItemShop)
-    private readonly itemShopRepository: Repository<ItemShop>,
+    private readonly itemShopRepo: Repository<ItemShop>,
     @InjectRepository(ItemType)
-    private readonly itemTypeRepository: Repository<ItemType>,
+    private readonly itemTypeRepo: Repository<ItemType>,
     @InjectRepository(ItemInventory)
-    private readonly itemInventoryRepository: Repository<ItemInventory>,
+    private readonly itemInvenRepo: Repository<ItemInventory>,
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private readonly userRepo: Repository<User>,
     private readonly doWithExceptions: DoWithExceptions,
     private readonly dataSource: DataSource
   ){}
 
+  /**
+   * 상점의 모든 아이템 목록 조회(보유 아이템 제외)
+   * @description 클라이언트 상점 탭에 따라서 아이템 종류가 다르기 때문에
+   *              요구 사항에 맞춰서 데이터 반환
+   * @param user_id 
+   * @returns 
+   */
   async getAllItems(user_id: number){
     // 유저가 소유한 아이템 번호 목록
-    const ItemsIds = await this.itemShopRepository.createQueryBuilder('ish')
-                                                  .leftJoin('item_inventory', 'iv', 'ish.item_id = iv.item_id')
-                                                  .select(['ish.item_id AS item_id'])
-                                                  .where('iv.user_id = :user_id', { user_id })
-                                                  .getRawMany();
+    const ItemsIds = await this.itemShopRepo.createQueryBuilder('ish')
+                                            .select(['ish.item_id AS item_id'])
+                                            .leftJoin('item_inventory', 'iv', 'ish.item_id = iv.item_id')
+                                            .where('iv.user_id = :user_id', { user_id })
+                                            .getRawMany();
     const ownItems = ItemsIds.map(data => data.item_id);
     // 현재 프로덕트가 가진 아이템 타입 정보
-    const types = await this.itemTypeRepository.createQueryBuilder('t')
-                                               .select([
-                                                 't.type_id   AS type_id'
-                                               , 't.type_name   AS type_name'
-                                               ])
-                                               .getRawMany();
+    const types = await this.itemTypeRepo.createQueryBuilder('t')
+                                         .select([
+                                           't.type_id   AS type_id'
+                                         , 't.type_name   AS type_name'
+                                         ])
+                                         .getRawMany();
     // 상점 아이템 조회                    
-    const query = await this.itemShopRepository.createQueryBuilder('ish')
-                                               .leftJoin('item_inventory', 'iv', 'iv.user_id = :user_id AND ish.item_id = iv.item_id', { user_id })
-                                               .leftJoin('item_type'     , 'it', 'ish.type_id = it.type_id')
-                                               .select([
-                                                 'ish.item_id    AS item_id'
-                                               , 'ish.item_name AS item_name'
-                                               , 'ish.item_cost AS item_cost'
-                                               , 'ish.item_path AS item_path'
-                                               ])
-                                               .where('ish.item_id NOT IN (:...ownItems)', { ownItems })
+    const query = await this.itemShopRepo.createQueryBuilder('ish')
+                                         .select([
+                                           'ish.item_id    AS item_id'
+                                         , 'ish.item_name AS item_name'
+                                         , 'ish.item_cost AS item_cost'
+                                         , 'ish.item_path AS item_path'
+                                         ])
+                                         .leftJoin('item_inventory', 'iv', 'iv.user_id = :user_id AND ish.item_id = iv.item_id', { user_id })
+                                         .leftJoin('item_type'     , 'it', 'ish.type_id = it.type_id')
+                                         .where('ish.item_id NOT IN (:...ownItems)', { ownItems })
     
-    let result = {};                                           
+    let result = {};                                
+    // 아이템 클라이언트 요청에 맞춰서 가공           
     for(const type of types){
       const type_id = type.type_id;
       const type_name = type.type_name;
@@ -62,44 +70,52 @@ export class ItemShopService {
     return { result, path: process.env.PUBLIC_IMAGE_PATH };
   }
 
+  /**
+   * 유저가 상점에서 아이템 구매
+   * @description 1. 아이템 가격 만큼 유저 캐시 차감
+   *              2. 유저 인벤토리에 구입한 아이템 추가
+   * @param user_id 
+   * @param item_id 
+   * @returns 
+   */
   async buyItem(user_id: number, item_id: number){
-    const queryRunner = this.dataSource.createQueryRunner();
+    const qr = this.dataSource.createQueryRunner();
     // 아이템 가격
-    const item_cost = await this.itemShopRepository.createQueryBuilder('ish')
-                                                   .select(['ish.item_cost AS item_cost'])
-                                                   .where('ish.item_id = :item_id', { item_id })
-                                                   .getRawOne();
+    const item_cost = await this.itemShopRepo.createQueryBuilder('ish')
+                                             .select(['ish.item_cost AS item_cost'])
+                                             .where('ish.item_id = :item_id', { item_id })
+                                             .getRawOne();
                                                    
     try {
-      await queryRunner.connect();
-      await queryRunner.startTransaction();
+      await qr.connect();
+      await qr.startTransaction();
 
-      // 유저 캐시 차감
-      const result = await this.userRepository.createQueryBuilder('u')
-                                              .update()
-                                              .set({ user_cash: () => `user_cash - ${item_cost.item_cost}` })
-                                              .where({ user_id })
-                                              .andWhere(`user_cash - ${item_cost.item_cost} >= 0`)
-                                              .execute();
+      // 1. 유저 캐시 차감 금액이 부족하다면 데이터 변화 없음
+      const result = await this.userRepo.createQueryBuilder('u')
+                                        .update()
+                                        .set({ user_cash: () => `user_cash - ${item_cost.item_cost}` })
+                                        .where({ user_id })
+                                        .andWhere(`user_cash - ${item_cost.item_cost} >= 0`)
+                                        .execute();
       if(result.affected === 0){
         throw this.doWithExceptions.NotEnoughCash;
       }
-
-      await this.itemInventoryRepository.createQueryBuilder('iv')
-                                        .insert()
-                                        .values({
-                                          user_id: user_id
-                                        , item_id: item_id
-                                        })
-                                        .execute();
+      // 2. 유저 인벤토리에 구입한 아이템 추가
+      await this.itemInvenRepo.createQueryBuilder('iv')
+                              .insert()
+                              .values({
+                                user_id: user_id
+                              , item_id: item_id
+                              })
+                              .execute();
                                       
-      await queryRunner.commitTransaction();
+      await qr.commitTransaction();
       return { result };
     } catch(err) {
-      await queryRunner.rollbackTransaction();
+      await qr.rollbackTransaction();
       throw new Error(err);
     } finally {
-      await queryRunner.release();
+      await qr.release();
     }
   }
 }
