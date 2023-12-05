1eaf7c8936801259439fb1e03767a16c56585bec
coding-jjun <psjbrian@gmail.com>
2023-12-05 18:16:17 +0900

    feat: add friendResponse, add todoImg in handleConfirmRequest

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -4,9 +4,10 @@ import { NotificationService } from './notification.service';
 import { Notification } from 'src/entities/notification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoWithExceptions } from 'src/utils/do-with-exception/do-with-exception';
+import { GatewayModule } from 'src/gateway/gateway.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification]), GatewayModule],
   controllers: [NotificationController],
   providers: [NotificationService, DoWithExceptions],
   exports: [NotificationService],
