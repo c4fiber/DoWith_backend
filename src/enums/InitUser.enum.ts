a308a8c2ddff3eb69a9270755e221f845ff13acc
cece <sojeong.lee017@gmail.com>
2023-12-06 04:31:45 +0900

    fix: init user_hp 변경

--- src/enums/InitUser.enum.ts
+++ src/enums/InitUser.enum.ts
@@ -1,11 +1,10 @@
 // 회원가입 시 초기화되는 유저 정보
 export enum InitUser {
+    hp = 10,
     cash = 0,
-    hp = 0,
     login_cnt = 1,
     login_seq = 1,
     pet_exp = 0,
     pet_id = 55,
-
     achi_id = 5, // 첫 로그인
 }
