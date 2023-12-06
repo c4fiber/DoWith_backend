e63122fbb571fe9859b2b79575ac79a93aba9aac
LeeInBok <bbock1214@gmail.com>
2023-12-07 02:10:25 +0900

    fix: rollback app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,30 @@
 import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TodoModule } from './features/todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { DoWithMiddlewareMiddleware } from 'src/utils/do-with-middleware.middleware';
+import { UserModule } from './features/user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupModule } from './features/group/group.module';
+import { RoutineModule } from './features/routine/routine.module';
+import { CommentModule } from './features/comment/comment.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { DoWithExceptionFilter } from './utils/do-with-exception.filter';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { FriendModule } from './features/friend/friend.module';
+import { CategoryModule } from './features/category/category.module';
 import { UtilsModule } from './utils/utils.module';
 import { DoWithInterceptor } from './utils/do-with-Interceptor';
 import { HttpModule } from '@nestjs/axios';
-import { GatewayModule } from './gateway/gateway.module';
-import { EntitiesModule } from './entities/entities.module';
-import { FriendModule } from './features/friend/friend.module';
-import { AchievementsModule } from './features/achievements/achievements.module';
-import { CategoryModule } from './features/category/category.module';
-import { ItemShopModule } from './features/item-shop/item-shop.module';
-import { AnnouncementModule } from './features/announcement/announcement.module';
-import { CommentModule } from './features/comment/comment.module';
-import { ItemInventoryModule } from './features/item-inventory/item-inventory.module';
 import { NotificationModule } from './features/notification/notification.module';
+import { GatewayModule } from './gateway/gateway.module';
+import { AchievementsModule } from './features/achievements/achievements.module';
+import { ItemInventoryModule } from './features/item-inventory/item-inventory.module';
+import { ItemShopModule } from './features/item-shop/item-shop.module';
 import { RoomModule } from './features/room/room.module';
-import { RoutineModule } from './features/routine/routine.module';
+import { EntitiesModule } from './entities/entities.module';
+import { AnnouncementModule } from './features/announcement/announcement.module';
 
 @Module({
   imports: [
@@ -46,23 +49,27 @@ import { RoutineModule } from './features/routine/routine.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    AuthModule,      // login Auth 
-    EntitiesModule,  // Entities
-    GatewayModule, 
-    UtilsModule,     // common
-    // Fixed Data Table
-    AchievementsModule,
+    // Common Module
+    UtilsModule,
+    // Fixed Table
     CategoryModule,
     ItemShopModule,
-
-    // APIs
-    AnnouncementModule,
-    CommentModule,
-    FriendModule,
-    ItemInventoryModule,
-    NotificationModule,
-    RoomModule,
+    AchievementsModule,
+    // API Module
+    UserModule,
+    TodoModule,
+    GroupModule,
     RoutineModule,
+    AuthModule,
+    NotificationModule,
+    FriendModule,
+    CommentModule,
+    ItemInventoryModule,
+    RoomModule,
+    GatewayModule,
+    EntitiesModule,
+    NotificationModule,
+    AnnouncementModule,
   ],
   controllers: [],
   providers: [
