c99303a8583a61151a8e12542e5e6168e3797d93
LeeInBok <bbock1214@gmail.com>
2023-12-12 11:11:03 +0900

    fix

--- src/features/room/room.service.ts
+++ src/features/room/room.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Room } from '../../entities/room.entity';
 import { DataSource } from 'typeorm';
 import { ItemInventory } from 'src/entities/item-inventory.entity';
 import { DoWithExceptions } from 'src/utils/do-with-exception';
+import { getIdsFromItems } from 'src/utils/PagingOptions';
 
 @Injectable()
 export class RoomService {
@@ -14,7 +15,7 @@ export class RoomService {
     @InjectRepository(Room)
     private readonly roomRepo: Repository<Room>,
     private dataSource: DataSource,
-    private readonly doWithException: DoWithExceptions,
+    private readonly dwExcept: DoWithExceptions,
   ) {}
 
   async isInInventory(user_id: number, item_id: number): Promise<boolean> {
@@ -27,39 +28,73 @@ export class RoomService {
     return result1 !== null;
   }
 
-  async overlap(user_id: number, items: number[]): Promise<any> {
+  // async overlap(user_id: number, items: number[]): Promise<any> {
     
+  //   const qr  = this.dataSource.createQueryRunner();
+  //   await qr.connect();
+  //   await qr.startTransaction();
+    
+  //   var result = [];
+
+  //   try {
+  //       // 기존의 room아이템 모두 삭제
+  //       await this.roomRepo.delete({ user_id });
+
+  //       for (const item_id of items) {
+  //         if (!(await this.isInInventory(user_id, item_id))) {
+  //           throw this.dwExcept.ItemNotInInventory;
+  //         }
+  //         if (await this.isInMyRoom(user_id, item_id)) {
+  //           throw this.dwExcept.ItemAlreadyInMyRoom;
+  //         }
+
+  //         // 보유한 아이템 + 방에 없음 -> 방에 추가
+  //         result.push(await this.roomRepo.save({ user_id, item_id }));
+  //       }
+  //     }
+  //     catch(err)  {
+  //       await qr.rollbackTransaction();
+  //       throw new DoWithExceptions().FailedToUpdateMyRoom;
+  //     }
+  //     finally {
+  //       qr.release();
+  //     };
+
+  //   return { result };
+  // }
+
+  async overlap(user_id: number, items: number[]): Promise<any> {
+    Logger.debug(items);
     const qr  = this.dataSource.createQueryRunner();
     await qr.connect();
     await qr.startTransaction();
     
-    var result = [];
+    let result = [];
 
     try {
         // 기존의 room아이템 모두 삭제
         await this.roomRepo.delete({ user_id });
+        const itemsInInv = await this.itemInventoryRepo.createQueryBuilder('iv')
+                                                       .select(['iv.item_id AS item_id'])
+                                                       .innerJoin('room', 'r', 'iv.item_id = r.item_id AND iv.user_id = r.user_id')
+                                                       .where({ user_id })
+                                                       .andWhere('iv.item_id IN (:...items)', { items })
+                                                       .getRawMany();
 
-        for (const item_id of items) {
-          if (!(await this.isInInventory(user_id, item_id))) {
-            throw this.doWithException.ItemNotInInventory;
-          }
-          if (await this.isInMyRoom(user_id, item_id)) {
-            throw this.doWithException.ItemAlreadyInMyRoom;
-          }
-
-          // 보유한 아이템 + 방에 없음 -> 방에 추가
+        for (const item of itemsInInv) {
+          const item_id = item.item_id;
           result.push(await this.roomRepo.save({ user_id, item_id }));
         }
-      }
-      catch(err)  {
+
+        qr.commitTransaction();
+
+        return { result };
+      } catch(err)  {
         await qr.rollbackTransaction();
-        throw new DoWithExceptions().FailedToUpdateMyRoom;
-      }
-      finally {
+        throw this.dwExcept.FailedToUpdateMyRoom;
+      } finally {
         qr.release();
       };
-
-    return { result };
   }
 
   async findAll(user_id: number) {
